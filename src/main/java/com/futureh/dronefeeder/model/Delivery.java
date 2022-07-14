37e6dcec9754be3ca628fdad1f1b622b84dcff53
alvesgf16 <alvesgabriel1994@gmail.com>
2022-07-14 17:03:31 -0300

    feat: application entities

--- src/main/java/com/futureh/dronefeeder/model/Delivery.java
+++ src/main/java/com/futureh/dronefeeder/model/Delivery.java
@@ -1,13 +1,55 @@
 package com.futureh.dronefeeder.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Delivery {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  @Column(name = "delivery_time")
+  private LocalDateTime deliveryTime;
+
+  private String status;
+
+  @ManyToOne
+  @JoinColumn(name = "drone_id")
+  private Drone drone;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDateTime getDeliveryTime() {
+    return deliveryTime;
+  }
+
+  public void setDeliveryTime(LocalDateTime deliveryTime) {
+    this.deliveryTime = deliveryTime;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public void setDrone(Drone drone) {
+    this.drone = drone;
+  }
 }
--- src/main/java/com/futureh/dronefeeder/model/Drone.java
+++ src/main/java/com/futureh/dronefeeder/model/Drone.java
@@ -1,13 +1,56 @@
 package com.futureh.dronefeeder.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Drone {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  private String latitude;
+
+  private String longitude;
+
+  @OneToMany(mappedBy = "drone", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Delivery> deliveries;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public List<Delivery> getDeliveries() {
+    return deliveries;
+  }
+
+  public void setDeliveries(List<Delivery> deliveries) {
+    this.deliveries = deliveries;
+  }
 }
