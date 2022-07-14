dd79a53d1c73e85a698265324c32002f262556e5
alvesgf16 <alvesgabriel1994@gmail.com>
2022-07-14 17:02:53 -0300

    refactor: rename application class

--- src/main/java/com/futureh/dronefeeder/DroneFeederApplication.java
+++ src/main/java/com/futureh/dronefeeder/DroneFeederApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DronefeederApplication {
+public class DroneFeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DronefeederApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DroneFeederApplication.class, args);
+  }
 
 }
