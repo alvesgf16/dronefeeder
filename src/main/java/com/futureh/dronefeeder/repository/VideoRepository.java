7a9147e69ccf2735670c99c708b1b79e5a60d5e1
Neander Menezes <neander2801@gmail.com>
2022-07-18 16:43:50 -0300

    changes(VideoRepository): changed getAllEntryNames query to show deliveryId and deliveryTime along with the videoId

--- src/main/java/com/futureh/dronefeeder/repository/VideoRepository.java
+++ src/main/java/com/futureh/dronefeeder/repository/VideoRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface VideoRepository extends JpaRepository<Video, Integer> {
   Video findByDescricao(String name);
 
-  @Query(nativeQuery = true, value="SELECT descricao, id FROM video")
+  @Query(nativeQuery = true, value="SELECT delivery.id, delivery.delivery_time, video_id FROM delivery\n" +
+      "JOIN video ON delivery.video_id = video.id")
   List<String> getAllEntryNames();
 }
