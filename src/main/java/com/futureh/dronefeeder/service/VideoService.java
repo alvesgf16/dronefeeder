b3b98c4dc1405ce41743fac48094a157d0dbe7dc
Neander Menezes <neander2801@gmail.com>
2022-07-18 16:44:19 -0300

    feat(VideoService): added interface VideoService for methods used in application

--- src/main/java/com/futureh/dronefeeder/service/VideoService.java
+++ src/main/java/com/futureh/dronefeeder/service/VideoService.java
@@ -1,30 +1,20 @@
 package com.futureh.dronefeeder.service;
 
 import com.futureh.dronefeeder.model.Video;
-import com.futureh.dronefeeder.repository.VideoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.futureh.dronefeeder.result.VideoGetAllResult;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-public class VideoService {
-  @Autowired
-  private VideoRepository repository;
+public interface VideoService {
 
-  public Video getVideoByDescricao(String descricao) {
-    return repository.findByDescricao(descricao);
+  Video getVideoById(Integer id);
+
+  void saveVideo(MultipartFile file, Integer deliveryId) throws IOException;
+
+  @Query(nativeQuery = true, value = "SELECT delivery.id, delivery.delivery_time, video_id FROM delivery\n" +
+      "JOIN video ON delivery.video_id = video.id")
+  public List<VideoGetAllResult> getAll();
   }
-
-  public void saveVideo(MultipartFile file, String descricao) throws IOException {
-    Video newVideo = new Video(file.getBytes(), descricao);
-    repository.save(newVideo);
-  }
-
-  public List<String> getAll() {
-    return repository.getAllEntryNames();
-  }
-}
